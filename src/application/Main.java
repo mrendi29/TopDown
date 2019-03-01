055df6841c6ba70f4c0b181eed67d838b600a08c
Endi Caushi <mrendi29@gmail.com>
2019-03-01 09:36:04 -0500

    Adjustments to main

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,15 +4,15 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Pane root = new Pane();
+			Scene scene = new Scene(root,800,800);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
@@ -23,4 +23,5 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
 }
