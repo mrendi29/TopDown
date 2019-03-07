a435028dd42b51b324e016f546244bde8733192d
Endi Caushi <mrendi29@gmail.com>
2019-03-07 17:18:28 -0500

    Setting up the environment

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			Pane root = new Pane();
-			Scene scene = new Scene(root,800,800);
+			Scene scene = new Scene(root,1440,900);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
