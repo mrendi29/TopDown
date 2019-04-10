2372ac74850236236e3f087258d46028f6d50c4f
Endi Caushi <caushie@wit.edu>
2019-04-10 18:04:51 -0400

    Revert "Code Refactor 3.0. Flickering still active"
    This reverts commit 15b217e02e8c9e1909cc4ffaf4099278ad39ff94

--- src/application/Physics.java
+++ src/application/Physics.java
@@ -25,7 +25,7 @@ public abstract class Physics {
 					bullet.setAlive(false);
 					enemy.setAlive(false);
 
-					// TODO: FIX RENDER PROBLEMS.
+					//TODO: FIX RENDER PROBLEMS.
 					Platform.runLater(() -> root.getChildren().removeAll(bullet.getGraphic(), enemy.getGraphic()));
 					Platform.runLater(() -> root.getChildren().remove(bullet.getIv()));
 					Platform.runLater(() -> root.getChildren().remove(enemy.getIv()));
