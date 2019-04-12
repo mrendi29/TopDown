95bb26baea5624876319662e694eec13eacab26f
zbk505 <43429646+zbk505@users.noreply.github.com>
2019-04-11 21:11:04 -0400

    added player image

--- src/application/GameTest.java
+++ src/application/GameTest.java
@@ -42,10 +42,10 @@ public class GameTest extends Application {
 
 		root = new Pane();
 
-		p = new Player(windowSizeX / 2, windowSizeY / 2, 30);
+		p = new Player(windowSizeX / 2, windowSizeY / 2,30, "ufo.png");
 		Text healthNode = new Text(50, windowSizeY - 100, "Lives: " + Integer.toString(p.getLives()));
 		healthNode.setFont(new Font(20));
-		root.getChildren().addAll(p.getGraphic(), healthNode);
+		root.getChildren().addAll(p.getGraphic(), healthNode, p.getImgv());
 
 		manager = SpawnManager.createInstance();
 		manager.setVariables(root, windowSizeX, windowSizeY);
--- src/application/Player.java
+++ src/application/Player.java
@@ -1,23 +1,37 @@
 package application;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class Player extends GameObject {
 
 	private boolean alive;
 	private int lives;
+	private Image img;
+	private ImageView imgv;
 
-	public Player(double x, double y, double radius) {
+	public Player(double x, double y, double radius, String filename) {
 		super(x, y, radius);
 
 		alive = true;
 		lives = 3;
 		
+		img = new Image(filename);
+		imgv = new ImageView(img);
+		
+		imgv.setLayoutX(x - 125);
+		imgv.setLayoutY(y - 125);
+		
 
 	}
 
 	public Player() {
-		this(200, 200, 20);
+		this(200, 200, 20,"");
 	}
-
+	public ImageView getImgv()
+	{
+		return imgv;
+	}
 	public boolean injure() {
 		lives--;
 		if (lives == 0) {
@@ -29,11 +43,6 @@ public class Player extends GameObject {
 	public int getLives() {
 		return lives;
 	}
-//	public Bullet shoot(double x, double y, double radius)
-//	{
-//		Bullet b = new Bullet(x, y, radius);
-//		
-//		return b;
-//	}
+
 
 }
