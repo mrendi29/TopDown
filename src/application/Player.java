38842fdea687560036c3e7159f5aa6a2de231dda
Endi Caushi <mrendi29@gmail.com>
2019-04-13 20:33:18 -0400

    Reverted Changes due to code incompatibility.

--- src/application/Player.java
+++ src/application/Player.java
@@ -1,17 +1,25 @@
 package application;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class Player extends GameObject {
 
 	private boolean alive;
 	private int lives;
+ 	private Image img= new Image("ufo.png",100,100,true,true);
+ 	private ImageView imgv;
 
 	public Player(double x, double y, double radius) {
 		super(x, y, radius);
 
 		alive = true;
 		lives = 3;
+		imgv = new ImageView();
+		imgv.setImage(img);
 		
-
+		imgv.setX(x);
+		imgv.setY(y);
 	}
 
 	public Player() {
@@ -29,6 +37,10 @@ public class Player extends GameObject {
 	public int getLives() {
 		return lives;
 	}
+	public ImageView getNode()
+ 	{
+ 		return imgv;
+ 	}
 //	public Bullet shoot(double x, double y, double radius)
 //	{
 //		Bullet b = new Bullet(x, y, radius);
